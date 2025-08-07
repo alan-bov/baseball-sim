75ddaa0f4b21f00399d404d6c3ea30784c8bfaea
Alan Bovee <abovee@pl-dev.com>
2025-08-07 08:50:09 -0400

    Created rust program for sim

---
+++ sim/target/debug/deps/sim-64745327ef6d8294.d
@@ -0,0 +1,5 @@
+C:\Users\abovee\Documents\Python Projects\BB Model\sim\target\debug\deps\sim-64745327ef6d8294.d: src\main.rs
+
+C:\Users\abovee\Documents\Python Projects\BB Model\sim\target\debug\deps\sim-64745327ef6d8294.exe: src\main.rs
+
+src\main.rs:
---
+++ sim/target/debug/sim.d
@@ -0,0 +1,1 @@
+C:\Users\abovee\Documents\Python\ Projects\BB\ Model\sim\target\debug\sim.exe: C:\Users\abovee\Documents\Python\ Projects\BB\ Model\sim\src\main.rs
